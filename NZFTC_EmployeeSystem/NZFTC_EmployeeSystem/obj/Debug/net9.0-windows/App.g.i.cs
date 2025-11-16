d6cddfebc17732304ccc3cfb898b8d1163db9ce0
Bailey <bailey.crawford123@gmail.com>
2025-11-17 09:25:47 +1300

    Added Employees To DB
    Employee Are Stored in Datebase

--- NZFTC_EmployeeSystem/NZFTC_EmployeeSystem/obj/Debug/net9.0-windows/App.g.i.cs
+++ NZFTC_EmployeeSystem/NZFTC_EmployeeSystem/obj/Debug/net9.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace NZFTC_EmployeeSystem {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace NZFTC_EmployeeSystem {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
         public static void Main() {
             NZFTC_EmployeeSystem.App app = new NZFTC_EmployeeSystem.App();
             app.InitializeComponent();
