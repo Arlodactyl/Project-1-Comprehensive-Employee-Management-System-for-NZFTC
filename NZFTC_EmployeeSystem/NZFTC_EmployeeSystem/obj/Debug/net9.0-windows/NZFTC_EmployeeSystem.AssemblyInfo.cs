d0e59df0d3d190a86b8350fe10423346ef61418f
Arlodactyl <102116019+Arlodactyl@users.noreply.github.com>
2025-11-17 11:34:19 +1300

    fully working - put db back
    g

--- NZFTC_EmployeeSystem/NZFTC_EmployeeSystem/obj/Debug/net9.0-windows/NZFTC_EmployeeSystem.AssemblyInfo.cs
+++ NZFTC_EmployeeSystem/NZFTC_EmployeeSystem/obj/Debug/net9.0-windows/NZFTC_EmployeeSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZFTC_EmployeeSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabd47cd336714e1d7fe1f818ad03b700668e773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2700f8d5ccc7add0413d28e5f209ed5fb34880c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZFTC_EmployeeSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZFTC_EmployeeSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
